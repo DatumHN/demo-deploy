81809e581f05878cd4cfdeeb0b08e004caea1255
DatumHN <melvin.cruz@datumredsoft.com>
2023-02-24 19:21:42 -0600

    Prueba

---
+++ src/main/java/com/datum/example/dao/ExampleDao.java
@@ -0,0 +1,12 @@
+package com.datum.example.dao;
+
+/**
+ * Esta interfaz debe heredar el CrudRepository enviando como parámetro la entidad.
+ * Ejemplo: extends CrudRepository<NombreEntidad,Long>
+ * @author Datum
+ * 
+ *
+ */
+public interface ExampleDao {
+
+}
